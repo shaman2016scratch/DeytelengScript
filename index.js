c046978de5d1f0ad56dc9ae4985e2fbcadc757c4
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-03 17:16:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+docData = {}
+docData.input = document.getElementById('input')
+docData.output = document.getElementById('output')
+theme = localStorage.getItem('theme')
+apps = localStorage.getItem('apps')
+app = {}
